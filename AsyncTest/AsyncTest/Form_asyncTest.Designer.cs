184fda9b42c7874fd2fdf87d8cb5ccd056dd7122
wisdomwei201804 <wisdom_wei@139.com>
2018-07-17 16:37:23 +0800

    测试项目，用于Thread,ThreadPool,Task, 到async await 的基本使用方法和理解。

---
+++ AsyncTest/AsyncTest/Form_asyncTest.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace AsyncTest
+{
+    partial class Form_asyncTest
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_log = new System.Windows.Forms.TextBox();
+            this.groupBox_log = new System.Windows.Forms.GroupBox();
+            this.button_run = new System.Windows.Forms.Button();
+            this.groupBox_setup = new System.Windows.Forms.GroupBox();
+            this.label_param = new System.Windows.Forms.Label();
+            this.textBox_param = new System.Windows.Forms.TextBox();
+            this.radioButton_async = new System.Windows.Forms.RadioButton();
+            this.radioButton_task = new System.Windows.Forms.RadioButton();
+            this.radioButton_threadpool = new System.Windows.Forms.RadioButton();
+            this.radioButton_thread = new System.Windows.Forms.RadioButton();
+            this.groupBox_log.SuspendLayout();
+            this.groupBox_setup.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox_log
+            // 
+            this.textBox_log.Location = new System.Drawing.Point(6, 20);
+            this.textBox_log.Multiline = true;
+            this.textBox_log.Name = "textBox_log";
+            this.textBox_log.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox_log.Size = new System.Drawing.Size(344, 137);
+            this.textBox_log.TabIndex = 0;
+            // 
+            // groupBox_log
+            // 
+            this.groupBox_log.Controls.Add(this.textBox_log);
+            this.groupBox_log.Location = new System.Drawing.Point(12, 92);
+            this.groupBox_log.Name = "groupBox_log";
+            this.groupBox_log.Size = new System.Drawing.Size(356, 163);
+            this.groupBox_log.TabIndex = 1;
+            this.groupBox_log.TabStop = false;
+            this.groupBox_log.Text = "Log";
+            // 
+            // button_run
+            // 
+            this.button_run.Location = new System.Drawing.Point(275, 49);
+            this.button_run.Name = "button_run";
+            this.button_run.Size = new System.Drawing.Size(75, 21);
+            this.button_run.TabIndex = 2;
+            this.button_run.Text = "Run";
+            this.button_run.UseVisualStyleBackColor = true;
+            this.button_run.Click += new System.EventHandler(this.button_run_Click);
+            // 
+            // groupBox_setup
+            // 
+            this.groupBox_setup.Controls.Add(this.label_param);
+            this.groupBox_setup.Controls.Add(this.textBox_param);
+            this.groupBox_setup.Controls.Add(this.radioButton_async);
+            this.groupBox_setup.Controls.Add(this.radioButton_task);
+            this.groupBox_setup.Controls.Add(this.radioButton_threadpool);
+            this.groupBox_setup.Controls.Add(this.radioButton_thread);
+            this.groupBox_setup.Controls.Add(this.button_run);
+            this.groupBox_setup.Location = new System.Drawing.Point(12, 6);
+            this.groupBox_setup.Name = "groupBox_setup";
+            this.groupBox_setup.Size = new System.Drawing.Size(356, 80);
+            this.groupBox_setup.TabIndex = 3;
+            this.groupBox_setup.TabStop = false;
+            this.groupBox_setup.Text = "Setup";
+            // 
+            // label_param
+            // 
+            this.label_param.AutoSize = true;
+            this.label_param.Location = new System.Drawing.Point(10, 53);
+            this.label_param.Name = "label_param";
+            this.label_param.Size = new System.Drawing.Size(65, 12);
+            this.label_param.TabIndex = 8;
+            this.label_param.Text = "Parameter:";
+            // 
+            // textBox_param
+            // 
+            this.textBox_param.Location = new System.Drawing.Point(81, 50);
+            this.textBox_param.Name = "textBox_param";
+            this.textBox_param.Size = new System.Drawing.Size(132, 21);
+            this.textBox_param.TabIndex = 7;
+            this.textBox_param.Text = "Here is a paramter.";
+            // 
+            // radioButton_async
+            // 
+            this.radioButton_async.AutoSize = true;
+            this.radioButton_async.Location = new System.Drawing.Point(219, 20);
+            this.radioButton_async.Name = "radioButton_async";
+            this.radioButton_async.Size = new System.Drawing.Size(89, 16);
+            this.radioButton_async.TabIndex = 6;
+            this.radioButton_async.TabStop = true;
+            this.radioButton_async.Text = "async await";
+            this.radioButton_async.UseVisualStyleBackColor = true;
+            // 
+            // radioButton_task
+            // 
+            this.radioButton_task.AutoSize = true;
+            this.radioButton_task.Location = new System.Drawing.Point(166, 20);
+            this.radioButton_task.Name = "radioButton_task";
+            this.radioButton_task.Size = new System.Drawing.Size(47, 16);
+            this.radioButton_task.TabIndex = 5;
+            this.radioButton_task.TabStop = true;
+            this.radioButton_task.Text = "Task";
+            this.radioButton_task.UseVisualStyleBackColor = true;
+            // 
+            // radioButton_threadpool
+            // 
+            this.radioButton_threadpool.AutoSize = true;
+            this.radioButton_threadpool.Location = new System.Drawing.Point(77, 20);
+            this.radioButton_threadpool.Name = "radioButton_threadpool";
+            this.radioButton_threadpool.Size = new System.Drawing.Size(83, 16);
+            this.radioButton_threadpool.TabIndex = 4;
+            this.radioButton_threadpool.TabStop = true;
+            this.radioButton_threadpool.Text = "ThreadPool";
+            this.radioButton_threadpool.UseVisualStyleBackColor = true;
+            // 
+            // radioButton_thread
+            // 
+            this.radioButton_thread.AutoSize = true;
+            this.radioButton_thread.Location = new System.Drawing.Point(12, 20);
+            this.radioButton_thread.Name = "radioButton_thread";
+            this.radioButton_thread.Size = new System.Drawing.Size(59, 16);
+            this.radioButton_thread.TabIndex = 3;
+            this.radioButton_thread.TabStop = true;
+            this.radioButton_thread.Text = "Thread";
+            this.radioButton_thread.UseVisualStyleBackColor = true;
+            // 
+            // Form_asyncTest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(380, 262);
+            this.Controls.Add(this.groupBox_setup);
+            this.Controls.Add(this.groupBox_log);
+            this.Name = "Form_asyncTest";
+            this.Text = "AsyncTest";
+            this.groupBox_log.ResumeLayout(false);
+            this.groupBox_log.PerformLayout();
+            this.groupBox_setup.ResumeLayout(false);
+            this.groupBox_setup.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_log;
+        private System.Windows.Forms.GroupBox groupBox_log;
+        private System.Windows.Forms.Button button_run;
+        private System.Windows.Forms.GroupBox groupBox_setup;
+        private System.Windows.Forms.Label label_param;
+        private System.Windows.Forms.TextBox textBox_param;
+        private System.Windows.Forms.RadioButton radioButton_async;
+        private System.Windows.Forms.RadioButton radioButton_task;
+        private System.Windows.Forms.RadioButton radioButton_threadpool;
+        private System.Windows.Forms.RadioButton radioButton_thread;
+    }
+}
+
---
+++ AsyncTest/AsyncTest/Form_asyncTest.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsyncTest
+{
+    public partial class Form_asyncTest : Form
+    {
+        public Form_asyncTest()
+        {
+            InitializeComponent();
+        }
+        private void ThreadTest()
+        {
+            textBox_log.AppendText(System.DateTime.Now.ToString() + " " + "ThreadTest main thread start ..." + "\r\n");
+            System.Console.WriteLine("ThreadTest main thread start ...");
+
+            //thread with no parameter.
+            //不带参数子线程
+            //var thread = new Thread(new ThreadStart(AsyncThread)); 
+            //thread.Start();
+
+            //thread with parameter.
+            //带参数子线程
+            var thread = new Thread(new ParameterizedThreadStart(AsyncThread));  
+            thread.Start(textBox_param.Text.ToString());
+
+            //wait for sub thread end.
+            //等待子线程结束
+            //thread.Join(); 
+
+            textBox_log.AppendText(System.DateTime.Now.ToString() + " " + "ThreadTest main thread end here." + "\r\n");
+            System.Console.WriteLine("ThreadTest main thread end here.");
+            System.Console.ReadLine();
+        }
+
+        private void AsyncThread(object param)
+        {
+            //call the mainform TextBox control between different threads.
+            //跨线程调用窗体控件。
+
+            //thread with no parameter.
+            //不带参数子线程
+            //SetText(System.DateTime.Now.ToString() + " " + "AsyncThread running here ..." + "\r\n");
+            //System.Console.WriteLine("AsyncThread running here ...");
+
+            //thread with parameter.
+            //带参数子线程
+            SetText(System.DateTime.Now.ToString() + " " + "AsyncThread running with parameter:" + param + "\r\n");            
+            System.Console.WriteLine("AsyncThread running with parameter:{0}", param);
+
+            //delay
+            //延迟输出
+            Thread.Sleep(100);
+        }
+
+        private void ThreadPoolTest()
+        {
+            textBox_log.AppendText(System.DateTime.Now.ToString() + " " + "ThreadPoolTest main thread start ..." + "\r\n");
+            System.Console.WriteLine("ThreadPoolTest main thread start ...");
+
+            AutoResetEvent mainEvent = new AutoResetEvent(false);
+            int workerThreads;
+            int portThreads;
+            ThreadPool.GetMaxThreads(out workerThreads, out portThreads);
+            textBox_log.AppendText(System.DateTime.Now.ToString() + " " + "\r\nMaximum worker threads:" + workerThreads.ToString() + "\r\nMaximum completion port threads:" + portThreads.ToString() + "\r\n");
+            Console.WriteLine("\nMaximum worker threads: \t{0}" +
+            "\nMaximum completion port threads: {1}",
+            workerThreads, portThreads);
+
+            var thread = new Thread(new ParameterizedThreadStart(AsyncThread));
+            thread.Start(textBox_param.Text.ToString());
+
+            ThreadPool.QueueUserWorkItem(new WaitCallback(AsyncThread), mainEvent);
+
+            ThreadPool.GetAvailableThreads(out workerThreads, out portThreads);
+            textBox_log.AppendText(System.DateTime.Now.ToString() + " " + "\r\nAvailable worker threads:" + workerThreads.ToString() + "\r\nAvailable completion port threads:" + portThreads.ToString() + "\r\n");
+            Console.WriteLine("\nAvailable worker threads: \t{0}" +
+            "\nAvailable completion port threads: {1}\n",
+            workerThreads, portThreads);
+
+            // Since ThreadPool threads are background threads, 
+            // wait for the work item to signal before ending Main.
+            textBox_log.AppendText(System.DateTime.Now.ToString() + " " + "Since ThreadPool threads are background threads, count 10 seconds wait for the work item to signal before ending Main." + "\r\n");
+            System.Console.WriteLine("Since ThreadPool threads are background threads, count 10 seconds wait for the work item to signal before ending Main.");
+            mainEvent.WaitOne(10000, false);
+
+            textBox_log.AppendText(System.DateTime.Now.ToString() + " " + "ThreadPoolTest main thread end here." + "\r\n");
+            System.Console.WriteLine("ThreadPoolTest main thread end here.");
+            System.Console.ReadLine();
+        }
+
+        private void TaskTest()
+        {
+            textBox_log.AppendText(System.DateTime.Now.ToString() + " " + "TaskTest main thread start ..." + "\r\n");
+            System.Console.WriteLine("TaskTest main thread start ...");
+            // new Task 创建方式-不带参数
+            //Task task = new Task(AsyncThread);
+            //task.Start();
+
+            // new Task 创建方式-带参数
+            //Task task=new Task(() =>  AsyncThread(10));
+
+            //Task.Factory 创建方式-不带参数
+            //Task task = Task.Factory.StartNew(AsyncThread);
+            //task.Start();
+
+            //Task.Factory 创建方式-带参数
+            //Task task = Task.Factory.StartNew(() => AsyncThread(10));
+            //task.Start();
+
+            //Task task = Task.Run(() => AsyncThread());
+            Task task = Task.Run(() => AsyncThread(textBox_param.Text.ToString()));
+            //task.Wait();
+
+            textBox_log.AppendText(System.DateTime.Now.ToString() + " " + "TaskTest main thread end here." + "\r\n");
+            System.Console.WriteLine("TaskTest main thread end here.");
+            System.Console.ReadLine();
+        }
+
+        private void AsyncAwaitTest()
+        {
+            textBox_log.AppendText(System.DateTime.Now.ToString() + " " + "AsyncAwaitTest main thread start ..." + "\r\n");
+            System.Console.WriteLine("AsyncAwaitTest main thread start ...");
+
+            AsyncAwaitThread();
+
+            textBox_log.AppendText(System.DateTime.Now.ToString() + " " + "AsyncAwaitTest main thread end here." + "\r\n");
+            System.Console.WriteLine("AsyncAwaitTest main thread end here.");
+            System.Console.ReadLine();
+        }
+
+        private async void AsyncAwaitThread()
+        {
+            await Task.Run(() =>
+            {
+                Thread.Sleep(100);
+                AsyncThread(textBox_param.Text.ToString());
+            });
+        }
+
+        private void button_run_Click(object sender, EventArgs e)
+        {
+            if(radioButton_thread.Checked)
+            {
+                ThreadTest();
+            }
+            else if(radioButton_threadpool.Checked)
+            {
+                ThreadPoolTest();
+            }
+            else if(radioButton_task.Checked)
+            {
+                TaskTest();
+            }
+            else if(radioButton_async.Checked)
+            {
+                AsyncAwaitTest();
+            }
+            else
+            {
+                textBox_log.AppendText(System.DateTime.Now.ToString() + " " + "Nothing selected ..." + "\r\n");
+            }
+        }
+
+        // This method demonstrates a pattern for making thread-safe
+        // calls on a Windows Forms control. 
+        //
+        // If the calling thread is different from the thread that
+        // created the TextBox control, this method creates a
+        // SetTextCallback and calls itself asynchronously using the
+        // Invoke method.
+        //
+        // If the calling thread is the same as the thread that created
+        // the TextBox control, the Text property is set directly. 
+
+        private void SetText(string text)
+        {
+            // InvokeRequired required compares the thread ID of the
+            // calling thread to the thread ID of the creating thread.
+            // If these threads are different, it returns true.
+            if (this.textBox_log.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetText);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.textBox_log.AppendText(text);
+            }
+        }
+        // This delegate enables asynchronous calls for setting
+        // the text property on a TextBox control.
+        delegate void SetTextCallback(string text);
+    }
+}
